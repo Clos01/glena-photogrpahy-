da0d14ebd66844f09b90c006336bb5a97dc9a898
Emmanuel6093 <elorenzo6093@gmail.com>
2023-04-22 12:44:26 -0400

    navbar length/width created, adding rest of content inside

--- Client/src/components/Navbar.jsx
+++ Client/src/components/Navbar.jsx
@@ -6,9 +6,17 @@ import { FaInstagram } from "react-icons/fa";
 
 function Navbar() {
   return (
-    <nav>
+    <nav className='bg-gray-800'>
 
-        
+        <div className='max-w-7xl mx-auto px-2 sm:px-6 lg:px8'>
+            <div className='relative flex items-center justify-between h-16'>
+                <div>
+                    <div>
+                        <img></img>
+                    </div>
+                </div>
+            </div>
+        </div>
 
     </nav>
   )
