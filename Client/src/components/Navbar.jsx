dc981974772d5dbd6c0465642e075e52afc6cb2e
Emmanuel6093 <elorenzo6093@gmail.com>
2023-04-22 13:58:53 -0400

    fully responsive on any screen size

--- Client/src/components/Navbar.jsx
+++ Client/src/components/Navbar.jsx
@@ -1,57 +1,88 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import { FaInstagram } from "react-icons/fa";
-
-// import GlendasLogo from '';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { FaInstagram, FaBars, FaTimes } from 'react-icons/fa';
 
 function Navbar() {
-    return (
-      <nav className="bg-gray-800">
-        <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px8">
-          <div className="relative flex items-center justify-between h-16">
-            <div className="flex-shrink-0 flex items-center">
-              {/* LOGO */}
-              <img
-                className="h-8 w-8"
-                src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg"
-                alt="logo"
-              />
-            </div>
-            <div className="hidden md:flex space-x-8">
-              <Link
-                to="/"
-                className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
-              >
-                Home
-              </Link>
-              <Link
-                to="/gallery"
-                className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
-              >
-                Gallery
-              </Link>
-              <Link
-                to="/contact"
-                className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
-              >
-                Contact
-              </Link>
-              <a
-                href="https://www.instagram.com/glenda.mendez.photography/?igshid=ZjE2NGZiNDQ%3D"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium flex items-center justify-center"
-              >
-                <FaInstagram className="mr-2" />
-                <span>Instagram</span>
-              </a>
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const closeMenu = () => {
+    setIsOpen(false);
+  };
+
+  const navLinks = (
+    <>
+      <Link
+        to="/"
+        onClick={closeMenu}
+        className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+      >
+        Home
+      </Link>
+      <Link
+        to="/gallery"
+        onClick={closeMenu}
+        className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+      >
+        Gallery
+      </Link>
+      <Link
+        to="/contact"
+        onClick={closeMenu}
+        className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+      >
+        Contact
+      </Link>
+      <a
+        href="https://www.instagram.com"
+        target="_blank"
+        rel="noopener noreferrer"
+        onClick={closeMenu}
+        className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium flex items-center justify-center"
+      >
+        <FaInstagram className="mr-2" />
+        <span>Instagram</span>
+      </a>
+    </>
+  );
+
+  return (
+    <nav className="bg-gray-800">
+      <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px8">
+        <div className="relative flex items-center justify-between h-16">
+          <div className="flex-shrink-0 flex items-center">
+            {/* LOGO */}
+            <img
+              className="h-8 w-8"
+              src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg"
+              alt="logo"
+            />
+          </div>
+          {/* Hamburger Icon (visible on small screens) */}
+          <div className="md:hidden">
+            <button onClick={toggleMenu} className="text-gray-300 hover:text-white focus:outline-none">
+              {isOpen ? <FaTimes /> : <FaBars />}
+            </button>
+          </div>
+          {/* Links (visible on larger screens) */}
+          <div className="hidden md:flex space-x-8">
+            {navLinks}
+          </div>
+          {/* Mobile Navigation Menu */}
+          <div className={`absolute top-16 right-0 w-full md:hidden ${isOpen ? 'block' : 'hidden'}`}>
+            <div className="bg-gray-700 p-4 flex flex-col items-center space-y-4">
+              {navLinks}
             </div>
           </div>
         </div>
-      </nav>
-    );
-  }
-  
-  
+      </div>
+    </nav>
+  );
+}
+
+
 
 export default Navbar;
