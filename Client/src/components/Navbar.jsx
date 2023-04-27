696b7e398b8a2bdc7eb6413a24212f8e38c4c74c
Emmanuel6093 <elorenzo6093@gmail.com>
2023-04-27 16:38:07 -0400

    fixed bug where logo flows outside of navbar

--- Client/src/components/Navbar.jsx
+++ Client/src/components/Navbar.jsx
@@ -20,7 +20,7 @@ function Navbar() {
       <Link
         to="/"
         onClick={closeMenu}
-        className=" text-gray-900 hover:bg-[#6bc489d2] hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+        className="text-gray-900 hover:bg-[#6bc489d2] hover:text-white px-3 py-2 rounded-md text-sm font-medium"
       >
         Home
       </Link>
@@ -59,13 +59,13 @@ function Navbar() {
   );
 
   return (
-    <nav className="bg-white">
-      <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px8">
-        <div className="relative flex items-center justify-between h-16">
+    <nav className="bg-white h-24">
+      <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+        <div className="relative flex items-center justify-between h-full">
           <div className="flex-shrink-0 flex items-center">
             {/* LOGO */}
             <img
-              className="h-32 w-32"
+              className="h-24 w-24"
               src={logo}
               alt="logo"
             />
@@ -77,11 +77,11 @@ function Navbar() {
             </button>
           </div>
           {/* Links (visible on larger screens) */}
-          <div className="hidden md:flex space-x-8">
+          <div className="hidden md:flex space-x-8 h-full">
             {navLinks}
           </div>
           {/* Mobile Navigation Menu */}
-          <div className={`absolute top-16 right-0 w-full md:hidden ${isOpen ? 'block' : 'hidden'}`}>
+          <div className={`absolute top-full right-0 w-full md:hidden ${isOpen ? 'block' : 'hidden'}`}>
             <div className="bg-gray-700 p-4 flex flex-col items-center space-y-4">
               {navLinks}
             </div>
@@ -91,7 +91,5 @@ function Navbar() {
     </nav>
   );
 }
-
-
 
 export default Navbar;
