7e322c3f57ba5a6e22f03d19e454a9d298f6666d
Carlos Rivas <Cjrivas00@gmail.com>
2023-05-09 10:56:22 -0400

    animation on contact

--- Client/src/pages/Contact.jsx
+++ Client/src/pages/Contact.jsx
@@ -1,6 +1,19 @@
 import React, { useState } from "react";
 import GlendaInsta from "../images/GlendaInsta.png";
+import { useSpring, animated } from 'react-spring';
+
 function ContactForm() {
+  const formAnimation = useSpring({
+    from: { opacity: 0, marginTop: -500 },
+    to: { opacity: 1, marginTop: 0 },
+  });
+  
+  const instaAnimation = useSpring({
+    from: { opacity: 0 },
+    to: { opacity: 1 },
+    delay: 500,
+  });
+  
   const [formState, setFormState] = useState({
     name: "",
     email: "",
@@ -24,14 +37,14 @@ function ContactForm() {
 
   return (
     <>
-      <div className="flex flex-col md:flex-row justify-center items-center border-black ">
-      <div className="w-full md:w-2/3 ">
-  <h1 className="text-5xl mt-[2rem]  pb-[3rem] text-center text-[#0f3649]">Get in Contact</h1>
-  <div className="  flex flex-col justify-center ">
-    <div className="relative py-3 sm:max-w-xl sm:mx-auto">
-      <div className="absolute inset-0 bg-gradient-to-r from-darkBlue via-lightGreen to-darkGreen shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl"></div>
-      <div className="relative w-full max-w-[30rem] px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20 ">
-                <form onSubmit={handleSubmit}>
+     <div className="flex flex-col md:flex-row justify-center items-center border-black ">
+            <animated.div style={formAnimation} className="w-full md:w-1/3 ">
+                <h1 className="text-5xl mt-[2rem]  pb-[3rem] text-center text-[#0f3649]">Get in Contact</h1>
+                <div className="flex flex-col justify-center ">
+                    <div className="relative py-3 sm:max-w-xl sm:mx-auto">
+                        <div className="absolute inset-0 bg-gradient-to-r from-darkBlue via-lightGreen to-darkGreen shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl"></div>
+                        <div className="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20 max-w-xl">
+                        <form onSubmit={handleSubmit}>
                   <div className="mb-5">
                     <label className="block text-sm font-medium text-gray-700">
                       Name
@@ -105,55 +118,45 @@ function ContactForm() {
                     </button>
                   </div>
                 </form>
-              </div>
-            </div>
-          </div>
-        </div>
-        <section>
-          <div className="max-w-screen-xl px-4 py-8 mx-auto sm:py-12 sm:px-6 lg:px-8">
-            <div className="grid grid-cols-1 gap-4 lg:grid-cols-3 lg:items-stretch">
-              <div className="grid p-2 rounded place-content-center sm:p-8">
-                <div className="max-w-md mx-auto text-center lg:text-left">
-                  <header className="lg:w-[15rem]  text-left   ">
-                    <h2 className="text-3xl font-bold text-[#498054c0] sm:text-4xl ">
-                      Thanks for visiting!
-                    </h2>
-
-                    <p className=" text-gray-500 text-lg mt-5 ">
-                      If you have any questions or concerns, please don't
-                      hesitate to get in touch with me.
-                    </p>
-                  </header>
+                        </div>
+                    </div>
                 </div>
-              </div>
-
-              <div className="lg:col-span-2 lg:py-8 w-full">
-  <ul className="grid grid-cols-1 gap-4 w-full">
-    <li className="w-full h-auto object-cover">
-      <a
-        href="https://www.instagram.com/glenda.mendez.photography/"
-        className="block group w-full"
-      >
-        <img
-          src={GlendaInsta}
-          alt=""
-          className="object-cover w-full h-[15rem] rounded aspect-square"
-        />
-
-        <div className="mt-3 text-center">
-          <h3 className="text-2xl font-medium text-[#498054] group-hover:underline group-hover:underline-offset-4 mt-[2rem]">
-            Join me on Instagram
-          </h3>
+            </animated.div>
+            <animated.div style={instaAnimation}>
+                <section>
+                    <div className="max-w-screen-xl px-4 py-8 mx-auto sm:py-12 sm:px-6 lg:px-8">
+                        <div className="grid grid-cols-1 gap-4 lg:grid-cols-3 lg:items-stretch">
+                            <div className="grid p-2 rounded place-content-center sm:p-8">
+                                <div className="max-w-md mx-auto text-center lg:text-left">
+                                    <header className="lg:w-[15rem]  text-left   ">
+                                        <h2 className="text-3xl font-bold text-[#498054c0] sm:text-4xl text-center ">
+                                            Thanks for visiting!
+                                        </h2>
+                                        <p className=" text-gray-500 text-lg mt-5 text-center ">
+                                            If you have any questions or concerns, please don't hesitate to get in touch with me.
+                                        </p>
+                                    </header>
+                                </div>
+                            </div>
+                            <div className="lg:col-span-2 lg:py-8 w-full">
+                                <ul className="grid grid-cols-1 gap-4 w-full">
+                                    <li className="w-full h-auto object-cover">
+                                        <a href="https://www.instagram.com/glenda.mendez.photography/" className="block group w-full">
+                                            <img src={GlendaInsta} alt="" className="object-cover w-full h-[15rem] rounded aspect-square" />
+                                            <div className="mt-3 text-center">
+                                                <h3 className="text-2xl font-medium text-[#498054] group-hover:underline group-hover:underline-offset-4 mt-[2rem]">
+                                                    Follow my Instagram 😁
+                                                </h3>
+                                            </div>
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            </animated.div>
         </div>
-      </a>
-    </li>
-  </ul>
-</div>
-
-            </div>
-          </div>
-        </section>
-      </div>
     </>
   );
 }
