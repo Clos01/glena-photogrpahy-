fc1850645e1e3610a9d84cc6e90b1995098749f7
Emmanuel6093 <elorenzo6093@gmail.com>
2023-05-25 18:38:40 -0400

    placed pricing content inside cards

--- Client/src/pages/Services.jsx
+++ Client/src/pages/Services.jsx
@@ -9,87 +9,87 @@ function Services() {
     {
       title: "Portrait: Outdoor",
       content: [
-        { images: "10-15", price: "$150" },
-        { images: "20-25", price: "$200" },
-        { images: "30-35", price: "$250" },
-        { images: "40-55", price: "$300" },
-        { images: "50-55", price: "$350" },
+        { image: "10-15", price: "$150" },
+        { image: "20-25", price: "$200" },
+        { image: "30-35", price: "$250" },
+        { image: "40-55", price: "$300" },
+        { image: "50-55", price: "$350" },
       ],
     },
     {
       title: 'Portrait: "In studio"',
       content: [
-        { images: "10-15", price: "$225" },
-        { images: "20-25", price: "$275" },
-        { images: "30-35", price: "$325" },
-        { images: "40-45", price: "$375" },
-        { images: "50-55", price: "$425" },
+        { image: "10-15", price: "$225" },
+        { image: "20-25", price: "$275" },
+        { image: "30-35", price: "$325" },
+        { image: "40-45", price: "$375" },
+        { image: "50-55", price: "$425" },
       ],
     },
     {
       title: "Baby Shower/Gender Reveal,",
       subtitle: "Bautizo, Confirmation, First Communion, Etc",
       content: [
-        { images: "10-15", price: "$150" },
-        { images: "20-25", price: "$200" },
-        { images: "30-35", price: "$250" },
-        { images: "40-55", price: "$300" },
-        { images: "50-55", price: "$350" },
+        { image: "10-15", price: "$150" },
+        { image: "20-25", price: "$200" },
+        { image: "30-35", price: "$250" },
+        { image: "40-55", price: "$300" },
+        { image: "50-55", price: "$350" },
       ],
     },
     {
       title: "Prom",
       content: [
-        { images: "10", price: "$125" },
-        { images: "15", price: "$150" },
-        { images: "20", price: "$175" },
-        { images: "25", price: "$200" },
-        { images: "30", price: "$225" },
+        { image: "10", price: "$125" },
+        { image: "15", price: "$150" },
+        { image: "20", price: "$175" },
+        { image: "25", price: "$200" },
+        { image: "30", price: "$225" },
       ],
     },
     {
       title: "Quince",
       content: [
-        { images: "15", price: "$175" },
-        { images: "20", price: "$200" },
-        { images: "25", price: "$225" },
-        { images: "30", price: "$250" },
-        { images: "35", price: "$275" },
-        { images: "40", price: "$300" },
-        { images: "45", price: "$325" },
-        { images: "50", price: "$350" },
-        { images: "55", price: "$375" },
-        { images: "60", price: "$400" },
+        { image: "15", price: "$175" },
+        { image: "20", price: "$200" },
+        { image: "25", price: "$225" },
+        { image: "30", price: "$250" },
+        { image: "35", price: "$275" },
+        { image: "40", price: "$300" },
+        { image: "45", price: "$325" },
+        { image: "50", price: "$350" },
+        { image: "55", price: "$375" },
+        { image: "60", price: "$400" },
       ],
     },
     {
       title: "Wedding",
       content: [
-        { images: "30", price: "$275" },
-        { images: "35", price: "$300" },
-        { images: "40", price: "$325" },
-        { images: "45", price: "$350" },
-        { images: "50", price: "$375" },
-        { images: "55", price: "$400" },
-        { images: "60", price: "$425" },
-        { images: "65", price: "$450" },
-        { images: "70", price: "$475" },
-        { images: "75", price: "$500" },
-        { images: "80", price: "$525" },
-        { images: "85", price: "$550" },
-        { images: "90", price: "$575" },
-        { images: "95", price: "$600" },
-        { images: "100", price: "$625" },
+        { image: "30", price: "$275" },
+        { image: "35", price: "$300" },
+        { image: "40", price: "$325" },
+        { image: "45", price: "$350" },
+        { image: "50", price: "$375" },
+        { image: "55", price: "$400" },
+        { image: "60", price: "$425" },
+        { image: "65", price: "$450" },
+        { image: "70", price: "$475" },
+        { image: "75", price: "$500" },
+        { image: "80", price: "$525" },
+        { image: "85", price: "$550" },
+        { image: "90", price: "$575" },
+        { image: "95", price: "$600" },
+        { image: "100", price: "$625" },
       ],
     },
     {
       title: "Extra Images",
       content: [
-        { images: "1", price: "$10" },
-        { images: "2", price: "$20" },
-        { images: "3", price: "$30" },
-        { images: "4", price: "$40" },
-        { images: "5", price: "$50" },
+        { image: "1", price: "$10" },
+        { image: "2", price: "$20" },
+        { image: "3", price: "$30" },
+        { image: "4", price: "$40" },
+        { image: "5", price: "$50" },
       ],
     },
     {
@@ -128,95 +128,74 @@ function Services() {
   };
 
   return (
-    <div className="container mx-auto">
+    <div className="container mx-auto mt-16" style={{ maxWidth: "90%" }}>
       <div className="relative">
-        <div className="carousel w-full flex justify-center">
-          <div className="slide active">
-            <div className="p-4 bg-white">
-              <div className="flex justify-center flex-col items-center">
-                <h3
-                  className="text-3xl font-bold mb-2"
-                  style={{ color: "#88b17e" }}
-                >
-                  {slides[currentSlide].title}
-                </h3>
-                <hr
-                  className="my-4"
-                  style={{
-                    borderTop: "2px solid #88b17e",
-                    width: "135%",
-                  }}
-                />
-              </div>
-              {slides[currentSlide].content.map((item, index) => (
-                <div key={index}>
-                  <div className="flex justify-between items-center mb-4">
-                    <div className="flex items-center">
-                      {currentSlide === slides.length - 2 ||
-                      currentSlide === slides.length - 1 ? (
-                        <p
-                          className="text-gray-700 mt-2 mr-2"
-                          style={{ color: "#0f3649" }}
-                        >
-                          {item.description}
-                        </p>
-                      ) : (
-                        <p
-                          className="text-gray-700 mt-2 mr-2"
-                          style={{ color: "#0f3649" }}
-                        >
-                          {item.images} images
-                        </p>
-                      )}
-                    </div>
-                    <div className="price-box ml-4">
-                      <p className="text-white px-4 py-1">{item.price}</p>
-                    </div>
-                  </div>
-                  <hr
-                    className="my-4"
-                    style={{
-                      borderTop: "2px solid #88b17e",
-                      marginLeft: "-2rem",
-                      marginRight: "-2rem",
-                    }}
-                  />
+        <div className="card p-4">
+          <div className="carousel w-full flex justify-center">
+            <div className="slide active">
+              <div className="bg-white rounded-lg shadow-md px-8 py-12">
+                <div className="text-center">
+                  <h3 className="text-2xl font-medium text-gray-900 mb-2">
+                    {slides[currentSlide].title}
+                  </h3>
                 </div>
-              ))}
+                <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-2">
+                  {slides[currentSlide].content.map((item, index) => (
+                    <div
+                      key={index}
+                      className={`card-item flex items-center justify-between bg-gray-100 p-4 rounded-lg ${
+                        slides[currentSlide].content.length === 1
+                          ? "mx-auto"
+                          : ""
+                      }`}
+                    >
+                      <div className="card-content flex-1">
+                        <div className="flex items-center">
+                          <div className="mr-4">
+                            <h4 className="text-lg font-medium text-gray-900 mt-2 mb-2">
+                              {`${item.image} Images`}
+                            </h4>
+                            <p className="text-gray-700">{item.description}</p>
+                          </div>
+                          <div className="card-price text-2xl font-medium text-gray-900">
+                            {item.price}
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  ))}
+                </div>
+              </div>
             </div>
           </div>
-        </div>
-        <div className="dots absolute bottom-0 left-0 right-0 flex justify-center">
-          {slides.map((_, index) => (
+          <div className="dots absolute bottom-0 left-0 right-0 flex justify-center">
+            {slides.map((_, index) => (
+              <button
+                key={index}
+                className={`dot ${currentSlide === index ? "active" : ""}`}
+                onClick={() => handleSlideChange(index)}
+              ></button>
+            ))}
+          </div>
+
+          <div className="arrows-container flex justify-between ">
             <button
-              key={index}
-              className={`dot ${currentSlide === index ? "active" : ""}`}
-              onClick={() => handleSlideChange(index)}
-            ></button>
-          ))}
+              className="prev absolute top-1/2 left-0 transform -translate-y-1/2 -translate-x-0 sm:inline-block"
+              onClick={goToPreviousSlide}
+            >
+              <FaChevronLeft className="h-8 w-8 text-gray-500" />
+            </button>
+            <button
+              className="next absolute top-1/2 right-0 transform -translate-y-1/3 translate-x-0 sm:inline-block"
+              onClick={goToNextSlide}
+            >
+              <FaChevronRight className="h-8 w-8 text-gray-500" />
+            </button>
+          </div>
         </div>
-  
-        {/* arrows */}
-        <div className="arrows-container flex justify-between">
-  <button
-    className="prev absolute top-1/2 left-10 transform -translate-y-1/2 translate-x-[-1rem] sm:inline-block"
-    onClick={goToPreviousSlide}
-  >
-    <FaChevronLeft />
-  </button>
-  <button
-    className="next absolute top-1/2 right-10 transform -translate-y-1/2 -translate-x-[-1rem] sm:inline-block"
-    onClick={goToNextSlide}
-  >
-    <FaChevronRight />
-  </button>
-</div>
-
-
       </div>
     </div>
   );
-  
-  
 }
+
 export default Services;
